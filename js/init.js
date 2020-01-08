4be990aec53841332cdac18cd08dddce579e19dc
EliteAppMakers-Ela <52504503+EliteAppMakers-Ela@users.noreply.github.com>
2020-01-09 00:49:13 +0530

    Add files via upload

---
+++ js/init.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+	$("#countdown").countdown({
+				date: "14 february 2020 00:00:00",
+				format: "off"
+			},
+
+			function() {
+				// callback function
+			});
+
+});
